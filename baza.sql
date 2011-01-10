33958f91b715752a862375d37df0d7c5b4c154c3
Krzysztof Karol W <dessident.agressor@gmail.com>
2011-01-10 18:31:28 +0100

    added image

--- baza.sql
+++ baza.sql
@@ -66,7 +66,7 @@ CREATE TABLE Usluga (
     id NUMBER PRIMARY KEY,
     koszt NUMBER NOT NULL CONSTRAINT koszt CHECK(koszt > 0),
     numer_listu VARCHAR2(20) NOT NULL,
-    potwierdzenie CHAR(1) NOT NULL CHECK(potwierdzenie IN ('Y','N'))
+    potwierdzenie CHAR(1) NOT NULL CHECK(potwierdzenie IN ('Y','N')),
     ilosc_transportow NUMBER NOT NULL
     CONSTRAINT sprawdz_transporty CHECK(ilosc_transportow > 0),
     stawka_vat NUMBER NOT NULL,
@@ -142,23 +142,28 @@ BEGIN
     WHERE Zlecenie.numer = :NEW.id_zlecenia;
 END;
 / 
-
-
+----------------------------------------------------------------------------------------- 
 CREATE OR REPLACE PACKAGE kurier AS
+/*
+ * Być może dodam jeszcze jakieś procedury w razie potrzeb - te mniej
+ * skomplikowane będę podawał wprost przez kod PHP bez definiowania ich w 
+ * pakiecie
+ */
     TYPE klient_refcur IS REF CURSOR RETURN klient%ROWTYPE;
+    TYPE zlecenie_refcur IS REF CURSOR;
     
-    PROCEDURE zalegajacyZPlatnoscia(klient_cur IN OUT klient_refcur);
+    PROCEDURE zalegajacyZPlatnoscia(klient_cur IN OUT kurier.klient_refcur);
     FUNCTION lacznyDochodZaOkres(data1 DATE, data2 DATE) RETURN NUMBER;
     PROCEDURE znajdzKlienta(nazwa_ IN VARCHAR2, miejscowosc_ IN VARCHAR2,
                             ulica_ IN VARCHAR2, klient_cur IN OUT klient_refcur);
+
+    PROCEDURE dodajUsluge(id_ IN NUMBER, koszt_ IN NUMBER, numer_listu_ IN VARCHAR2,
+                          potwierdzenie_ IN NUMBER, ilosc_transportow_ IN NUMBER, 
+                          stawka_vat_ IN NUMBER, id_odbiorcy_ IN NUMBER, 
+                          id_pracownika_ NUMBER, numer_zlecenia_ IN VARCHAR,
+                          nadawca_ NUMBER DEFAULT NULL);
     
-    PROCEDURE dodajUsluge(id IN NUMBER, koszt IN NUMBER, numer_listu IN VARCHAR2(20),
-                          typ_uslugi IN VARCHAR2(20), potwierdzenie IN NUMBER,
-                          ilosc_transportow IN NUMBER, stawka_vat IN NUMBER,
-                          id_odbiorcy IN NUMBER, id_pracownika NUMBER,
-                          numer_zlecenia IN VARCHAR, nadawca NUMBER DEFAULT NULL);
-                          
-    PROCEDURE usunTowar(id_kasowanego NUMBER, nazwa_tabeli VARCHAR2, z_usuwalne CHAR(1));
+    PROCEDURE usunTowar(id_kasowanego NUMBER, nazwa_tabeli VARCHAR2, z_usuwalne CHAR);
     PROCEDURE usunZlecenie(numer_kasowanego VARCHAR2);
     
 END kurier;
@@ -173,22 +178,21 @@ CREATE OR REPLACE PACKAGE BODY kurier AS
         WHERE SUBSTR(UPPER(nazwa), 1, LENGTH(nazwa_)) = UPPER(nazwa_)
             OR SUBSTR(UPPER(miejscowosc), 1, LENGTH(miejscowosc_)) = UPPER(miejscowosc_)
             OR SUBSTR(UPPER(ulica), 1, LENGTH(ulica_)) = UPPER(ulica_);
-END;
+    END;
 -----------------------------------------------------------------------------------------
-    PROCEDURE zalegajacyZPlatnoscia(klient_cur IN OUT klient_refcur)
-    DECLARE
+    PROCEDURE zalegajacyZPlatnoscia(klient_cur IN OUT kurier.klient_refcur) IS
         dzis DATE;
     BEGIN
         SELECT sysdate INTO dzis FROM dual;
         OPEN klient_cur FOR SELECT k.id, k.nazwa, k.numer_budynku, k.numer_lokalu,
-                                   k.kod_pocztowy, k.miejscowosc, k.ulica, k.nazwa_skrocona, 
-                                   k.nip  
+                                   k.kod_pocztowy, k.miejscowosc, k.ulica, 
+                                   k.nazwa_skrocona, k.nip  
                             FROM Klient k LEFT JOIN Zlecenie z ON k.id = z.id_nadawcy
-                            WHERE z.data_platnosci < dzis;
-END;
+                            WHERE z.data_platnosci IS NULL OR
+                            (z.data_platnosci IS NOT NULL AND z.data_platnosci < dzis);
+    END;
 -----------------------------------------------------------------------------------------
-    FUNCTION lacznyDochodNettoZaOkres(data1 DATE, data2 DATE) RETURN NUMBER IS
-    DECLARE
+    FUNCTION lacznyDochodZaOkres(data1 DATE, data2 DATE) RETURN NUMBER IS
         res NUMBER;
         dummy NUMBER;
         CURSOR koszt_cur IS
@@ -204,76 +208,70 @@ END;
         RETURN res;
     END lacznyDochodZaOkres;
 -----------------------------------------------------------------------------------------    
-    PROCEDURE dodajUsluge(id_ IN NUMBER, koszt_ IN NUMBER, numer_listu_ IN VARCHAR2(20),
-                          typ_uslugi_ IN VARCHAR2(20), potwierdzenie_ IN NUMBER,
-                          ilosc_transportow_ IN NUMBER, stawka_vat_ IN NUMBER,
-                          id_odbiorcy_ IN NUMBER, id_pracownika_ NUMBER, 
-                          numer_zlecenia IN VARCHAR, nadawca NUMBER DEFAULT NULL) IS
-    DECLARE
+    PROCEDURE dodajUsluge(id_ IN NUMBER, koszt_ IN NUMBER, numer_listu_ IN VARCHAR2,
+                          potwierdzenie_ IN NUMBER, ilosc_transportow_ IN NUMBER, 
+                          stawka_vat_ IN NUMBER, id_odbiorcy_ IN NUMBER, 
+                          id_pracownika_ NUMBER, numer_zlecenia_ IN VARCHAR,
+                          nadawca_ NUMBER DEFAULT NULL) IS
+                        
         czy_jest Zlecenie.numer%TYPE;
         teraz DATE;
     BEGIN
     -- prosty wrapper, który dodaje usluge i jesli nie ma zlecenia do ktorego sie
     -- odwoluje to je tworzy (o ile nadawca jest podany)
         SELECT sysdate INTO teraz FROM dual;
-        SELECT COUNT(*) INTO czy_jest FROM Zlecenie WHERE numer = numer_zlecenia;
-        IF czy_jest = 0 AND nadawca IS NOT NULL THEN
-            INSERT INTO Zlecenie VALUES (numer_zlecenia, teraz, 0, 'przelew',
-                                         teraz + 14, nadawca);
+        SELECT COUNT(*) INTO czy_jest FROM Zlecenie WHERE numer = numer_zlecenia_;
+        IF czy_jest = 0 AND nadawca_ IS NOT NULL THEN
+            INSERT INTO Zlecenie VALUES (numer_zlecenia_, 'N', teraz, '0', 'przelew',
+                                         teraz + 14, nadawca_, NULL);
         END IF;
-        INSERT INTO Usluga VALUES (id_, koszt_, numer_listu_, typ_uslugi_, potwierdzenie_,
-                                   ilosc_transportow_, stawka_vat_, numer_zlecenia,
-                                id_odbiorcy_, id_pracownika_);
-    END;
+        INSERT INTO Usluga VALUES (id_, koszt_, numer_listu_, potwierdzenie_,
+                                   ilosc_transportow_, stawka_vat_, numer_zlecenia_,
+                                   id_odbiorcy_, id_pracownika_);
+    END dodajUsluge;
 -----------------------------------------------------------------------------------------    
-    PROCEDURE usunTowar(id_kasowanego NUMBER, nazwa_tabeli VARCHAR2, z_usuwalne CHAR(1))
-    IS
-    DECLARE
-        id_uslugi_ Usluga.id%TYPE;
-        id_zlecenia_ Zleceni.numer%TYPE;
-        ile_uslug_w_zleceniu NUMBER;
+    PROCEDURE usunTowar(id_kasowanego NUMBER, nazwa_tabeli VARCHAR2, z_usuwalne CHAR) IS
+        id_uslugi_ NUMBER;
+        ilosc_uslug NUMBER;
+        zlecenie_ VARCHAR2(20);
+        id_zlecen zlecenie_refcur;
     BEGIN
-    --prosty wrapper usuwajacy 'towar' czyli Paczke, Terminal lub PrzewozOsob
+    --prosty wrapper usuwajacy 'towar' czyli Paczke, Terminal lub Przewoz_Osob
+        EXECUTE IMMEDIATE --moge dac selecta bo usluga jest 1-1 z Paczka, Terminalem itp
+            'SELECT id_uslugi FROM '||nazwa_tabeli||' WHERE id = '||id_kasowanego
+        INTO id_uslugi_;
+        OPEN id_zlecen FOR SELECT id_zlecenia FROM Usluga WHERE id = id_uslugi_;
+        DELETE FROM Usluga WHERE id = id_uslugi_;
         EXECUTE IMMEDIATE
-            'SELECT id_uslugi FROM ' || nazwa_tabeli
-            || ' WHERE id = id_kasowanego' INTO id_uslugi_;
-        SELECT id_zlecenia INTO id_zlecenia FROM Usluga WHERE id = id_uslugi_;
-        DELETE FROM Usluga WHERE id = id_uslugi;
-        EXECUTE IMMEDIATE
-            'DELETE FROM ' || nazwa_tabeli ||' WHERE id = id_kasowanego';
-        SELECT COUNT(*) INTO  ile_uslug_w_zleceniu 
-            FROM Usluga WHERE id_zlecenia = id_zlecenia_;
-        IF (ile_uslug_w_zleceniu < 1 AND z_usuwalne = 'Y') THEN
-            DELETE FROM Zlecenie WHERE numer = id_zlecenia_;
-        END IF;
+            'DELETE FROM '||nazwa_tabeli||' WHERE id = '||id_kasowanego;
+        LOOP
+            FETCH id_zlecen INTO zlecenie_;
+            EXIT WHEN id_zlecen%NOTFOUND;
+            SELECT COUNT(*) INTO  ilosc_uslug 
+                FROM Usluga WHERE id_zlecenia = zlecenie_;
+            IF (ilosc_uslug < 1 AND z_usuwalne = 'Y') THEN
+                DELETE FROM Zlecenie WHERE numer = zlecenie_;
+            END IF;
+        END LOOP;
+        CLOSE id_zlecen;
     END usunTowar;
 -----------------------------------------------------------------------------------------
     PROCEDURE usunZlecenie(numer_kasowanego VARCHAR2) IS
+        CURSOR usuwane_uslugi IS
+            SELECT id FROM Usluga WHERE id_zlecenia = numer_kasowanego;
+        id_uslugi_ NUMBER; 
     BEGIN
-    --niszczy zlecenie i wszystkei wchodzace w jego sklad uslugi
-        FOR id_uslugi_ IN 
-            (SELECT id FROM Usluga WHERE id_zlecenia = numer_kasowanego) LOOP
+    --niszczy zlecenie i wszystkie wchodzace w jego sklad uslugi
+        LOOP
+            FETCH usuwane_uslugi INTO id_uslugi_;
+            EXIT WHEN usuwane_uslugi%NOTFOUND;
             DELETE FROM Terminal WHERE id_uslugi = id_uslugi_;
             DELETE FROM Paczka WHERE id_uslugi = id_uslugi_;
-            DELETE FROM PrzewozOsob WHERE id_uslugi = id_uslugi_;
+            DELETE FROM Przewoz_Osob WHERE id_uslugi = id_uslugi_;
         END LOOP;
+        
+        DELETE FROM Usluga WHERE id_zlecenia = numer_kasowanego;
         DELETE FROM Zlecenie WHERE numer = numer_kasowanego;
     END usunZlecenie;
------------------------------------------------------------------------------------------
-    /*PROCEDURE usunTerminal(id_kasowanego NUMBER) IS
-    DECLARE
-        id_uslugi_ NUMBER;
-        id_zlecenia_ VARCHAR2;
-        ile_uslug_w_zleceniu NUMBER;
-    BEGIN
-        SELECT id_uslugi INTO id_uslugi_ FROM Terminal WHERE id = id_kasowanego;
-        SELECT id_zlecenia INTO id_zlecenia FROM Usluga WHERE id = id_uslugi_;
-        DELETE FROM Usluga WHERE id = id_uslugi;
-        DELETE FROM Terminal WHERE id = id_kasowanego;
-        SELECT COUNT(*) INTO  ile_uslug_w_zleceniu FROM Usluga WHERE id_zlecenia = id_zlecenia_;
-        IF (ile_uslug_w_zleceniu < 1) THEN
-            DELETE FROM Zlecenie WHERE numer = id_zlecenia_;
-        END IF;
-    END usunTerminal;*/
-
 END kurier;
+/
